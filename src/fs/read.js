4a941a39e0f8430d1c33a5fd81d629fc34ff7862
amgstrider <amgstrider@gmail.com>
2024-01-26 18:26:54 +0400

    feat: read

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,18 @@
+import fs from 'fs';
+import path from 'path';
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const __filename = path.join(__dirname, 'files/fileToRead.txt');
+
 const read = async () => {
-    // Write your code here 
+    try {
+        const content = await fs.promises.readFile(__filename, 'utf-8');
+        console.log(content);
+    } catch {
+        throw new Error('FS operation failed');
+    }
 };
 
 await read();
