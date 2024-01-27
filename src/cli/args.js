6e0e6c976533fa4b6466f41ff8bdd9accd6ca702
amgstrider <amgstrider@gmail.com>
2024-01-27 18:40:47 +0400

    feat: args function

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,13 @@
 const parseArgs = () => {
-    // Write your code here 
+    const argvArr = process.argv.slice(2);
+    const preparedArr = argvArr.reduce((acc, curr, index) => {
+        if (curr.startsWith('--')) {
+            acc.push(`${curr.slice(2)} is ${argvArr[index + 1]}`);
+        }
+        return acc;
+    }, []);
+
+    console.log(preparedArr.join('; '));
 };
 
 parseArgs();
