38fb00e1257f118355996725b3f8452c55dc5f8b
amgstrider <amgstrider@gmail.com>
2024-01-28 10:37:44 +0400

    feat: write function

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,24 @@
+import fs from 'fs';
+import path from 'path';
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const __toReadFilename = path.join(__dirname, 'files/fileToRead.txt');
+const __toWriteFilename = path.join(__dirname, 'files/fileToWrite.txt');
+
 const write = async () => {
-    // Write your code here 
+    const readStream = fs.createReadStream(__toReadFilename);
+    const writeStream = fs.createWriteStream(__toWriteFilename);
+
+    readStream.on('data', chunk => {
+        writeStream.write(chunk);
+    });
+
+    readStream.on('end', () => {
+        writeStream.end();
+        console.log('\nDone');
+    });
 };
 
 await write();
