e8b94d6b2666e638ce71d827be1fa3513924b9e0
amgstrider <amgstrider@gmail.com>
2024-01-29 14:32:50 +0400

    featt: child processes

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,16 @@
+import { fork } from 'child_process';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const __scriptFilename = path.join(__dirname, 'files/script.js');
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+    fork(__scriptFilename, args, {
+        stdio: [process.stdin, process.stdout, process.stderr, 'ipc']
+    });
 };
 
 // Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess([1, 2, 3, 4, 5]);
