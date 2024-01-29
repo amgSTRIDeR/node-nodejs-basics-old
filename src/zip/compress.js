7102e2232a4560b016224d7571c5f631dddc0e31
amgstrider <amgstrider@gmail.com>
2024-01-29 10:08:58 +0400

    feat: compress function

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,23 @@
+import zlib from 'zlib';
+import path from 'path';
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import fs from 'fs';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const __fileToCompress = path.join(__dirname, 'files/fileToCompress.txt');
+const __compressedFile = path.join(__dirname, 'files/archive.gz');
+
 const compress = async () => {
-    // Write your code here 
+    const gzip = zlib.createGzip();
+    const source = fs.createReadStream(__fileToCompress);
+    const destination = fs.createWriteStream(__compressedFile);
+
+    source.pipe(gzip).pipe(destination);
+
+    destination.on('finish', () => {
+        console.log('File compressed');
+    });
 };
 
 await compress();
