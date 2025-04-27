64302e2afdeb658ae096f01098fcb8b48e22c9af
amgstrider <amgstrider@gmail.com>
2025-04-27 20:56:16 +0400

    feat: rename

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,37 @@
+import path from 'path';
+import { fileURLToPath } from 'url';
+import fs from 'fs';
+import logColoredMessage from '../common/colors.js';
+
+const __filename = fileURLToPath(import.meta.url); 
+const __dirname = path.dirname(__filename); 
+
 const rename = async () => {
-    // Write your code here 
+    const wrongFileName = path.join(__dirname, 'files', 'wrongFileName.txt');
+    const correctFileName = path.join(__dirname, 'files',  'properFilename.md');
+try {
+    await fs.promises.access(wrongFileName);
+
+    try {
+        await fs.promises.access(correctFileName);
+        logColoredMessage(`File ${correctFileName} already exists`, 'yellow');
+        logColoredMessage('FS operation failed', 'red');
+    } catch(err) {
+        if(err.code === 'ENOENT') {
+            await fs.promises.rename(wrongFileName, correctFileName);
+            logColoredMessage(`File ${wrongFileName} was successfully rename to ${correctFileName}`, 'green');
+        }
+    }
+
+
+   
+} catch(err) {
+    logColoredMessage(err, 'yellow')
+    logColoredMessage('FS operation failed', 'red');
+}
+   
+
+
 };
 
 await rename();
