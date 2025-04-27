fe873280f90ba64d4bd03d20b7fffed6d1592c06
amgstrider <amgstrider@gmail.com>
2025-04-27 18:33:18 +0400

    feat: color messages

---
+++ src/common/colors.js
@@ -0,0 +1,15 @@
+const COLORS = {
+    RED: '\x1b[31m',
+    GREEN: '\x1b[32m',
+    YELLOW: '\x1b[33m',
+    BLUE: '\x1b[34m',
+    MAGENTA: '\x1b[35m',
+    CYAN: '\x1b[36m',
+    WHITE: '\x1b[37m',
+    RESET: '\x1b[0m',
+};
+
+export default function logColoredMessage(message, color = 'white') {
+    const currentColor = COLORS[color.toUpperCase()] ?? COLORS.WHITE;
+    console.log(`${currentColor}${message}${COLORS.RESET}`)
+};
