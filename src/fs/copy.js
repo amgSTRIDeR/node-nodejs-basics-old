673ff3a77408eb9260efbe5dc8a05a376ce89dbf
amgstrider <amgstrider@gmail.com>
2025-04-27 18:44:07 +0400

    fix: error text

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,3 +1,11 @@
+import path from 'path';
+import { fileURLToPath } from 'url';
+import fs from 'fs';
+import logColoredMessage from '../common/colors.js';
+
+const __filename = fileURLToPath(import.meta.url); 
+const __dirname = path.dirname(__filename); 
+
 const copy = async () => {
     // Write your code here 
 };
