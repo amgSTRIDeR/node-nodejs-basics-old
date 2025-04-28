82b1b3cfe329e497e078e17585ad475abf164098
amgstrider <amgstrider@gmail.com>
2025-04-29 00:22:51 +0400

    feat: transform

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,20 @@
+import process from 'process';
+import logColoredMessage from '../common/colors.js';
+
+
 const transform = async () => {
-    // Write your code here 
+    logColoredMessage('For exit press "ctrl + c"', 'yellow');
+    logColoredMessage('Write text to reverse:', 'green');
+
+    process.stdin.on('data', (chunk) => {
+
+        process.stdout.write(`\nReversed text: ${chunk.toString().split('').reverse().join('')}\n\n`);
+    })
+
+    process.on('SIGINT', () => {
+        logColoredMessage('\nSee you', 'green');
+        process.exit();
+    });
 };
 
 await transform();
