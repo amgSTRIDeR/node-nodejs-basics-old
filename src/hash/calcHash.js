a3dd74065a6d98e8b32d0cb9ab97b5de9359ac9a
amgstrider <amgstrider@gmail.com>
2025-04-28 22:41:29 +0400

    feat: hash

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,30 @@
+import path from 'path';
+import { fileURLToPath } from 'url';
+import fs from 'fs';
+import crypto from 'crypto';
+import logColoredMessage from '../common/colors.js';
+
+const __fileName = fileURLToPath(import.meta.url)
+const __dirName = path.dirname(__fileName);
+const filePath = path.join(__dirName, 'files', 'fileToCalculateHashFor.txt');
+
 const calculateHash = async () => {
-    // Write your code here 
+    const readStream = fs.createReadStream(filePath);
+    const hash = crypto.createHash('sha256');
+
+    readStream.on('data', (chunk) => {
+        hash.update(chunk);
+    })
+
+    readStream.on('end', () => {
+        logColoredMessage(`Hash is ${hash.digest('hex')}`, 'green');
+    })
+
+    readStream.on('error', (err) => {
+        logColoredMessage(`Oops, there are some error: ${err}`, 'red');
+    })
+
+
 };
 
 await calculateHash();
