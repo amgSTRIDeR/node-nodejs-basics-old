5a81d8ed144e792d335b8c55bcd3c540720fcd33
amgstrider <amgstrider@gmail.com>
2025-04-28 18:38:29 +0400

    feat: file delete

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,23 @@
+import path from 'path';
+import { fileURLToPath } from 'url';
+import fs from 'fs';
+import logColoredMessage from '../common/colors.js';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const fileName = 'fileToRemove.txt';
+const filePath = path.join(__dirname, 'files', fileName);
+
 const remove = async () => {
-    // Write your code here 
+    try {
+        await fs.promises.rm(filePath);
+        logColoredMessage(`File ${fileName} has been deleted`, 'green');
+    } catch (err) {
+        if(err.code === 'ENOENT') {
+            logColoredMessage(`File ${filePath} does not exist`, 'yellow');
+        }
+        logColoredMessage('FS operation failed', 'red');
+    }
 };
 
 await remove();
