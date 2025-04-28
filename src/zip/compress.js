c9470815b3328776f0a90b7167d81947cda159c3
amgstrider <amgstrider@gmail.com>
2025-04-29 00:36:00 +0400

    feat: zlib

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,30 @@
+import path from 'path';
+import { fileURLToPath } from 'url';
+import logColoredMessage from '../common/colors.js';
+import fs from 'fs';
+import zlib from 'zlib';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const fileToCompressPath = path.join(__dirname, 'files', 'fileToCompress.txt');
+const compressedFilePath = path.join(__dirname, 'files', 'archive.gz');
+
+
 const compress = async () => {
-    // Write your code here 
+    const zlibInfo = zlib.createGzip();
+
+    const readableStream = fs.createReadStream(fileToCompressPath);
+    const writableStream = fs.createWriteStream(compressedFilePath);
+
+    readableStream.pipe(zlibInfo).pipe(writableStream);
+
+    readableStream.on('end', () => {
+        logColoredMessage(`File ${fileToCompressPath} has been compressed to ${compressedFilePath}`, 'green');
+    })
+
+    readableStream.on('error', (err) => {
+        logColoredMessage(`Error occured: ${err}`, 'red');
+    })
 };
 
 await compress();
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,31 @@
+import path from 'path';
+import { fileURLToPath } from 'url';
+import logColoredMessage from '../common/colors.js';
+import fs from 'fs';
+import zlib from 'zlib';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const fileToDecompressPath = path.join(__dirname, 'files', 'archive.gz');
+const decompressedFilePath = path.join(__dirname, 'files', 'fileToCompress.txt');
+
+
+
 const decompress = async () => {
-    // Write your code here 
+        const zlibInfo = zlib.createUnzip();
+    
+        const readableStream = fs.createReadStream(fileToDecompressPath);
+        const writableStream = fs.createWriteStream(decompressedFilePath);
+    
+        readableStream.pipe(zlibInfo).pipe(writableStream);
+    
+        readableStream.on('end', () => {
+            logColoredMessage(`File ${fileToDecompressPath} has been decompressed to ${decompressedFilePath}`, 'green');
+        })
+    
+        readableStream.on('error', (err) => {
+            logColoredMessage(`Error occured: ${err}`, 'red');
+        })
 };
 
 await decompress();
